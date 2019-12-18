9066902dce3fc3e6a70fae6ce3696f801fe0da65
jimCresswell <jim.cresswell@gmail.com>
2019-12-18 18:08:42 +0000

    Add get, delete and add methods to the provider data model

--- provider/data/pollinator_support_list.js
+++ provider/data/pollinator_support_list.js
@@ -1,4 +1,5 @@
 const cloneDeep = require('lodash.clonedeep');
+const remove = require('lodash.remove');
 
 /**
  * A list of pollinator support species and associated methods.
@@ -25,6 +26,40 @@ class PollinatorSupportList {
    */
   reset() {
     this.data = cloneDeep(this.initialData);
+  }
+
+  /**
+   * Add a plant.
+   * @param {Object} plant A plant to add to the list.
+   * @return {int} The new ID of the inserted plant object.
+   */
+  add(plant) {
+    const newId = this.data.plants.length + 1;
+    const plantCopy = cloneDeep(plant);
+
+    plantCopy.id = newId;
+    this.data.plants.push(plantCopy);
+
+    return newId;
+  }
+
+  /**
+   * Delete a plant from the list by ID.
+   * @param  {int} id The ID to remove.
+   * @return {Object[]}    The removed plant object(s).
+   */
+  deleteById(id) {
+    return remove(this.data.plants, (plant) => plant.id === id);
+  }
+
+  /**
+   * Get a plant by it's ID.
+   * @param  {int} id The numeric ID to look for.
+   * @return {Object}    A plant
+   */
+  getById(id) {
+    /** @todo add type checking on the id */
+    return this.data.plants.find((plant) => plant.id === id);
   }
 }
 
--- provider/data/pollinator_support_list.test.js
+++ provider/data/pollinator_support_list.test.js
@@ -25,16 +25,49 @@ describe('The pollinator support list', function () {
     expect(plants).to.be.an('array').with.lengthOf(4);
   });
 
-  it('can be cleared', function () {
+  it('can be cleared.', function () {
     this.list.clear();
     const { plants } = this.list.data;
     expect(plants).to.be.an('array').that.is.empty;
   });
 
-  it('can be reset', function () {
+  it('can be reset.', function () {
     this.list.clear();
     this.list.reset();
     const { plants } = this.list.data;
     expect(plants).to.be.an('array').with.lengthOf(4);
+  });
+
+  it('can get plants by id.', function () {
+    const plant1 = this.list.getById(1);
+    expect(plant1).to.have.property('species', 'Rhamnus cathartica');
+  });
+
+  it('cannot get plants by id that do not exist.', function () {
+    const plant1 = this.list.getById(9999);
+    expect(plant1).to.be.undefined;
+  });
+
+  it('can delete plants by ID.', function () {
+    const { plants } = this.list.data;
+    const idToRemove = plants.length; // Remove the last plant in the list.
+    const originalNames = plants.map((plant) => plant.common_name);
+    const expectedNames = originalNames.slice(0, plants.length - 1); // Remove the last name.
+
+    this.list.deleteById(idToRemove);
+
+    const remainingNames = plants.map((plant) => plant.common_name);
+    expect(remainingNames).to.have.members(expectedNames);
+  });
+
+  it('can have extra plants added.', function () {
+    const newPlantData = {
+      common_name: 'Snozcumber',
+      species: 'Cucumis dahlius',
+      link: 'https://en.wikipedia.org/wiki/The_BFG',
+    };
+    const id = this.list.add(newPlantData);
+    const newPlant = this.list.getById(id);
+    expect(newPlant).to.have.property('common_name', 'Snozcumber');
   });
 });
