157a5300d3f882370d4e588e82082aef348b3918
jimCresswell <jim.cresswell@gmail.com>
2020-01-07 19:14:34 +0000

    Plug front-end into back-end
    * Plug front-end into back-end
    * Add automatic server restart on file change.

--- consumer/app/routes/ui.js
+++ consumer/app/routes/ui.js
@@ -1,17 +1,24 @@
 const express = require('express');
 
+const axios = require('axios');
+
 /* eslint-disable new-cap */
 const router = express.Router();
 /* eslint-enable new-cap */
 
 
-router.get('/', (req, res) => {
-  const viewData = {
-    mainTitle: 'Hello plants',
-    content: 'Plants are great',
-  };
-
-  res.render('index', viewData);
+router.get('/', (req, res, next) => {
+  axios.get('http://localhost:3001/plants')
+    .then((response) => {
+      const viewData = {
+        mainTitle: 'Pollinator Supporting Plants',
+        plants: response.data.plants,
+      };
+      res.render('index', viewData);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
